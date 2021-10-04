794b54ba89bacaac941804a819b94cb6e7fc4521
Halle Countryman <hkcountryman@gmail.com>
2021-10-04 11:01:06 -0700

    adding some cors headers?

--- netlify/functions/trek.js
+++ netlify/functions/trek.js
@@ -3,6 +3,11 @@ const trekkers = require('../../data.json');
 exports.handler = async function(event, context) {
     return {
         statusCode: 200,
+        headers: {
+            "Access-Control-Allow-Headers" : "*",
+            "Access-Control-Allow-Origin": "https://615b3f3ddc63e220665f857f--naughty-dubinsky-1c07af.netlify.app/com",
+            "Access-Control-Allow-Methods": "*"
+        },
         body: JSON.stringify(trekkers)
     };
 }
